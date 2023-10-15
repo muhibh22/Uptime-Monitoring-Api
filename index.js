2fc69a4737c28e1c76fc796a9846074d4deed55f
muhibh22 <muhibh22@gmail.com>
2023-10-15 20:50:30 +0600

    commit

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-HTMLBodyElement
+//dependencies
+
+const http=require('http');
+
+
+//appobject - module scaffolding
+
+const app={};
+
+//configuration
+app.config={
+    port:3000
+};
+
+app.createServer=()=>{
+    const server=http.createServer(app.handleReqRes);
+    server.listen(app.config.port, ()=>{
+        console.log(`listening to port ${app.config.port}`);
+    } );
+};
+
+//handle Request Response
+app.handleReqRes=(req, res)=>{
+res.end('Hello Programmers');
+};
+
+//start the server
+app.createServer();
+
+
